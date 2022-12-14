6eae4e05d8494f1ae2ead9b4028e6c0b53d264f5
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-14 22:58:22 +0100

    part 2

--- 2022/day2/part2.c
+++ 2022/day2/part2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#pragma string name day 2 part 1
+#pragma string name day 1 part 2
 
 #include "input.h"
 
 int main() {
-    printf("Day 2 part 1\n");
+    printf("Day 2 part 2\n");
 
     int ans = 0;
 
@@ -14,7 +14,7 @@ int main() {
         char a = c[0] - 'A';
         char b = c[2] - 'X';
 
-        ans += (b + 1) + (b - a + 4) % 3 * 3;
+        ans += b * 3 + (b + a + 2) % 3 + 1;
     }
 
     printf("ans: %d\n", ans);
