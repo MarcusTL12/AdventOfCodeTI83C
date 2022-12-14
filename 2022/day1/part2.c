b9e9063e9549d57f34a6a2498f6273f7c53841d1
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-14 22:08:33 +0100

    part 2

--- 2022/day1/part2.c
+++ 2022/day1/part2.c
@@ -3,17 +3,23 @@
 
 #pragma string name day 1 part 2
 
-#include "ex1.h"
+#include "input.h"
 
 int main() {
-    printf("Day 1 part 1\n");
+    printf("Day 1 part 2\n");
 
-    long cursum = 0;
-    long curmax = 0;
+    int32_t cursum = 0, curmax[] = {0, 0, 0};
 
     for (char *c = input; *c; c++) {
         if (*c == '\n') {
-            curmax = cursum > curmax ? cursum : curmax;
+            for (char i = 0; i < 3; i++) {
+                if (cursum > curmax[i]) {
+                    int32_t tmp = cursum;
+                    cursum = curmax[i];
+                    curmax[i] = tmp;
+                }
+            }
+
             cursum = 0;
         } else {
             cursum += atol(c);
@@ -21,7 +27,21 @@ int main() {
         }
     }
 
-    printf("ans: %ld\n", curmax);
+    for (char i = 0; i < 3; i++) {
+        if (cursum > curmax[i]) {
+            int32_t tmp = cursum;
+            cursum = curmax[i];
+            curmax[i] = tmp;
+        }
+    }
+
+    int32_t ans = 0;
+
+    for (char i = 0; i < 3; i++) {
+        ans += curmax[i];
+    }
+
+    printf("ans: %ld\n", ans);
 
     getkey();
     return 0;
