30a988772dae0a8d1dd7145536baa4e1ed65359f
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-15 00:18:29 +0100

    Day 4 part 1 works

---
+++ 2022/day4/part1.c
@@ -0,0 +1,40 @@
+#include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma string name day 4 part 1
+
+#include "input.h"
+
+int main() {
+    printf("Day 4 part 1\n");
+
+    uint16_t ans = 0;
+
+    for (char *c = input; *c; c++) {
+        uint8_t rs[2][2];
+
+        rs[0][0] = atoi(c);
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c)) c++;
+        rs[0][1] = atoi(c);
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c)) c++;
+        rs[1][0] = atoi(c);
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c)) c++;
+        rs[1][1] = atoi(c);
+        while (isdigit(*c)) c++;
+
+        if (rs[0][0] <= rs[1][0] && rs[0][1] >= rs[1][1] ||
+            rs[0][0] >= rs[1][0] && rs[0][1] <= rs[1][1]) {
+            ans++;
+        }
+    }
+
+    printf("%d\n", ans);
+
+    getkey();
+    return 0;
+}
---
+++ 2022/day4/part2.c
@@ -0,0 +1,42 @@
+#include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma string name day 4 part 2
+
+#include "ex1.h"
+
+int main() {
+    printf("Day 4 part 2\n");
+
+    uint16_t ans = 0;
+
+    for (char *c = input; *c; c++) {
+        uint8_t rs[2][2];
+
+        rs[0][0] = atoi(c);
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c)) c++;
+        rs[0][1] = atoi(c);
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c)) c++;
+        rs[1][0] = atoi(c);
+        while (isdigit(*c)) c++;
+        while (!isdigit(*c)) c++;
+        rs[1][1] = atoi(c);
+        while (isdigit(*c)) c++;
+
+        if ((rs[0][0] >= rs[1][0] && rs[0][0] <= rs[1][1]) ||
+            (rs[0][1] >= rs[1][0] && rs[0][1] <= rs[1][1]) ||
+            (rs[1][0] >= rs[0][0] && rs[1][0] <= rs[0][1]) ||
+            (rs[1][1] >= rs[0][0] && rs[1][1] <= rs[0][1])) {
+            ans++;
+        }
+    }
+
+    printf("%d\n", ans);
+
+    getkey();
+    return 0;
+}
