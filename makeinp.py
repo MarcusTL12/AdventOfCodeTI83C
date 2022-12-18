fe2cda3c45f64ed0de67609ce5edf533b5742190
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-18 11:50:03 +0100

    Setup works

--- makeinp.py
+++ makeinp.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 
-os.makedirs("build", exist_ok=True)
+def makeinp(year, day, inpname):
+    inp_path = f"{year}/day{day}/{inpname}"
+    out_path = f"build/{inpname}.c"
 
-year = sys.argv[1]
-day = sys.argv[2]
-inpname = "input"
-if len(sys.argv) >= 4:
-    inpname = sys.argv[3]
+    i_type = "unsigned int"
+    i_width = 2
 
-inp_path = f"{year}/day{day}/{inpname}"
-out_path = f"{year}/day{day}/{inpname}.c"
+    with open(inp_path) as inp_f:
+        inp_s = inp_f.read()
 
-i_type = "unsigned int"
-i_width = 2
+    while len(inp_s) % i_width != 0:
+        inp_s += '\0'
 
-with open(inp_path) as inp_f:
-    inp_s = inp_f.read()
-
-while len(inp_s) % i_width != 0:
-    inp_s += '\0'
-
-with open(out_path, "w") as out_f:
-    out_f.write(i_type + " input[] = {")
-    for i in range(0, len(inp_s), i_width):
-        x = 0
-        for j in range(i_width):
-            x += 2**(8 * j) * ord(inp_s[i + j])
-        out_f.write(f"{x}, ")
-    out_f.write("0};\n")
-    # out_f.write("char *input = (char *)input_raw;\n")
+    with open(out_path, "w") as out_f:
+        out_f.write(i_type + " input[] = {")
+        for i in range(0, len(inp_s), i_width):
+            x = 0
+            for j in range(i_width):
+                x += 2**(8 * j) * ord(inp_s[i + j])
+            out_f.write(f"{x}, ")
+        out_f.write("0};\n")
--- setup.py
+++ setup.py
@@ -1,3 +1,63 @@
 #!/usr/bin/env python3
 
+import subprocess
+import sys
+import os
+import shutil
 
+from makeinp import makeinp
+
+year = sys.argv[1]
+day = sys.argv[2]
+part = sys.argv[3]
+
+if len(sys.argv) >= 5:
+    inpname = sys.argv[4]
+else:
+    inpname = "input"
+
+ansi = False
+compiler = "sdcc"
+# compiler = "sccz80"
+
+opt = 3
+
+speed = ','.join(["sub32", "add32", "sub16", "lshift32", "rshift32",
+                 "inlineints", "ucharmult", "intcompare", "longcompare"])
+# speed = ""
+
+flags = ["+ti83"]
+
+if ansi:
+    flags.append("-clib=ansi")
+
+flags.append(f"-compiler={compiler}")
+
+if compiler == "sccz80" and len(speed) > 0:
+    flags.append(f"--opt-code-speed={speed}")
+
+if compiler == "sccz80":
+    flags.append(f"-O{opt}")
+else:
+    flags.append(f"-SO{opt}")
+
+flags = ' '.join(flags)
+
+cmake_command = ' '.join([
+    "cmake ..",
+    "-DCMAKE_C_COMPILER=zcc",
+    f"-DCMAKE_C_FLAGS='{flags}'",
+    f"-DYEAR={year}",
+    f"-DDAY={day}",
+    f"-DPART={part}",
+    f"-DINPUT={inpname}",
+])
+
+shutil.rmtree("build")
+os.makedirs("build")
+
+makeinp(year, day, inpname)
+
+os.chdir("build")
+
+subprocess.call(cmake_command, shell=True)
