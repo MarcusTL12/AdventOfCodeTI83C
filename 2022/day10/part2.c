2dbcf77070fc32bd47cc53535b0dc60b37823770
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-23 10:22:30 +0100

    Day 10 part 2

---
+++ 2022/day10/part2.c
@@ -0,0 +1,53 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <graphics.h>
+
+// #include "abs_m.h"
+
+extern char input[];
+
+static int16_t sx, sy;
+
+static void push_pixel(int16_t x) {
+    if (abs(sx - x) <= 1) {
+        plot(sx, sy);
+    } else {
+        unplot(sx, sy);
+    }
+
+    sx++;
+    if (sx == 40) {
+        sx = 0;
+        sy++;
+    }
+}
+
+int main() {
+    printf("Day %d part %d\n", 10, 2);
+
+    sx = 0;
+    sy = 0;
+
+    int16_t x = 1;
+
+    for (char *c = input; *c; c++) {
+        if (*c == 'a') {
+            c += 5;
+
+            int8_t n = atoi(c);
+            while (*c != '\n') c++;
+
+            push_pixel(x);
+            push_pixel(x);
+
+            x += n;
+        } else {
+            c += 4;
+            push_pixel(x);
+        }
+    }
+
+    getkey();
+    return 0;
+}
---
+++ util/abs_m.h
@@ -0,0 +1,6 @@
+#ifndef __abs_m__
+#define __abs_m__
+
+#define abs_m(x) x < 0 ? -x : x
+
+#endif
